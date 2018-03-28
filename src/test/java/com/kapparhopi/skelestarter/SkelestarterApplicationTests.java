5a8e2941eb412093ffb7b02f446965a8773e6241
John Doe <john@sample.com>
2018-03-28 11:56:13 -0400

    Added bootstrap and jquery dependencies

---
+++ src/test/java/com/kapparhopi/skelestarter/SkelestarterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kapparhopi.skelestarter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SkelestarterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
