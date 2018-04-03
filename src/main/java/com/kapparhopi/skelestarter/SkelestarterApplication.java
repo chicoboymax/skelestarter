208ef4ad0107c70efa1d5d92bf8f44c4fce6dfd9
Maxime Drouin <chicoboymax@gmail.com>
2018-04-03 16:59:28 -0400

    Refactored Main class

--- src/main/java/com/kapparhopi/skelestarter/SkelestarterApplication.java
+++ src/main/java/com/kapparhopi/skelestarter/SkelestarterApplication.java
@@ -1,12 +1,61 @@
 package com.kapparhopi.skelestarter;
 
+import com.kapparhopi.skelestarter.backend.persistence.domain.backend.Role;
+import com.kapparhopi.skelestarter.backend.persistence.domain.backend.User;
+import com.kapparhopi.skelestarter.backend.persistence.domain.backend.UserRole;
+import com.kapparhopi.skelestarter.backend.service.PlanService;
+import com.kapparhopi.skelestarter.backend.service.UserService;
+import com.kapparhopi.skelestarter.enums.PlansEnum;
+import com.kapparhopi.skelestarter.enums.RolesEnum;
+import com.kapparhopi.skelestarter.utils.UserUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @SpringBootApplication
-public class SkelestarterApplication {
+@Slf4j
+public class SkelestarterApplication implements CommandLineRunner {
+
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private PlanService planService;
+
+    @Value("${webmaster.username}")
+    private String webmasterUsername;
+
+    @Value("${webmaster.password}")
+    private String webmasterPassword;
+
+    @Value("${webmaster.email}")
+    private String webmasterEmail;
+
 
     public static void main(String[] args) {
         SpringApplication.run(SkelestarterApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+
+        log.info("Creating Basic and Pro plans in the database...");
+        planService.createPlan(PlansEnum.BASIC.getId());
+        planService.createPlan(PlansEnum.PRO.getId());
+
+        User user = UserUtils.createBasicUser(webmasterUsername, webmasterEmail);
+        user.setPassword(webmasterPassword);
+        Set<UserRole> userRoles = new HashSet<>();
+        userRoles.add(new UserRole(user, new Role(RolesEnum.ADMIN)));
+        log.debug("Creating user with username {}", user.getUsername());
+        userService.createUser(user, PlansEnum.PRO, userRoles);
+        log.info("User {} created", user.getUsername());
     }
 }
